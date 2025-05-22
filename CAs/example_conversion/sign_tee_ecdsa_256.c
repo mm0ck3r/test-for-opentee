f7789ce02e928edb04178b5b52eef6aa961213be
김성진 <mm0ck3r@gmail.com>
2025-05-23 04:37:06 +0900

    commit - Jang
    Jang's code

--- CAs/example_conversion/sign_tee_ecdsa_256.c
+++ CAs/example_conversion/sign_tee_ecdsa_256.c
@@ -25,21 +25,21 @@
 #include "sign_ecdsa_256_ctrl.h"
 
 /* Data buffer sizes */
-#define DATA_SIZE	256
-#define SHA1_SIZE	20
+#define DATA_SIZE   256
+#define SHA1_SIZE   20
 #define SHA256_SIZE 32
 
 /* Hash TA command IDs for this applet */
 #define HASH_DO_FINAL 0x00000001
-#define SIGN_DO_FINAL	0x00000002
+#define SIGN_DO_FINAL   0x00000002
 
 /* Hash algoithm */
-#define HASH_MD5	0x00000001
-#define HASH_SHA1	0x00000002
-#define HASH_SHA224	0x00000003
-#define HASH_SHA256	0x00000004
-#define HASH_SHA384	0x00000005
-#define HASH_SHA512	0x00000006
+#define HASH_MD5   0x00000001
+#define HASH_SHA1   0x00000002
+#define HASH_SHA224   0x00000003
+#define HASH_SHA256   0x00000004
+#define HASH_SHA384   0x00000005
+#define HASH_SHA512   0x00000006
 
 /* Blockchain Network */
 #define NETWORK_BITCOIN 0x0
@@ -58,151 +58,151 @@ void print_hex(const char *label, const uint8_t *data, size_t len) {
 
 int main()
 {
-	TEEC_Context context;
-	TEEC_Session session;
-	TEEC_Operation operation = {0};
-	TEEC_SharedMemory in_mem = {0};
-	TEEC_SharedMemory out_mem = {0};
-	TEEC_SharedMemory sign_input_mem = {0};
-	TEEC_SharedMemory sign_output_mem = {0};
-	TEEC_SharedMemory key = {0};
-	TEEC_Result tee_rv;
+   TEEC_Context context;
+   TEEC_Session session;
+   TEEC_Operation operation = {0};
+   TEEC_SharedMemory in_mem = {0};
+   TEEC_SharedMemory out_mem = {0};
+   TEEC_SharedMemory sign_input_mem = {0};
+   TEEC_SharedMemory sign_output_mem = {0};
+   TEEC_SharedMemory key = {0};
+   TEEC_Result tee_rv;
 
-	char data[DATA_SIZE];
-	uint8_t sha256[SHA256_SIZE];
-	unsigned char sig[72];
-	uint8_t fuck[96];
+   char data[DATA_SIZE];
+   uint8_t sha256[SHA256_SIZE];
+   unsigned char sig[72];
+   uint8_t fuck[96];
 
-	int i;
-	
-	printf("\nSTART: example ECDSA for BITCOIN app\n");
+   int i;
+   
+   printf("\nSTART: example ECDSA for BITCOIN app\n");
 
-	/* Initialize data stuctures */
-	memset((void *)&in_mem, 0, sizeof(in_mem));
-	memset((void *)&out_mem, 0, sizeof(out_mem));
-	memset((void *)&operation, 0, sizeof(operation));
-	strncpy(data, BITCOIN_MESSAGE, BITCOIN_MESSAGE_LEN);
-	memset(sha256, 0, SHA256_SIZE);
-	memset(sig, 0, 72);
+   /* Initialize data stuctures */
+   memset((void *)&in_mem, 0, sizeof(in_mem));
+   memset((void *)&out_mem, 0, sizeof(out_mem));
+   memset((void *)&operation, 0, sizeof(operation));
+   strncpy(data, BITCOIN_MESSAGE, BITCOIN_MESSAGE_LEN);
+   memset(sha256, 0, SHA256_SIZE);
+   memset(sig, 0, 72);
 
-	/*
-	 * Initialize context towards TEE
-	 */
-	printf("Initializing context: ");
-	tee_rv = TEEC_InitializeContext(NULL, &context);
-	if (tee_rv != TEEC_SUCCESS) {
-		printf("TEEC_InitializeContext failed: 0x%x\n", tee_rv);
-		goto end_1;
-	} else {
-		printf("initialized\n");
-	}
+   /*
+    * Initialize context towards TEE
+    */
+   printf("Initializing context: ");
+   tee_rv = TEEC_InitializeContext(NULL, &context);
+   if (tee_rv != TEEC_SUCCESS) {
+      printf("TEEC_InitializeContext failed: 0x%x\n", tee_rv);
+      goto end_1;
+   } else {
+      printf("initialized\n");
+   }
 
-	/*
-	 * Open session towards Digest TA
-	 */
-	tee_rv = TEEC_OpenSession(&context, &session,
-				  &uuid, TEEC_LOGIN_PUBLIC,
-				  NULL, NULL, NULL);
-	if (tee_rv != TEEC_SUCCESS) {
-		printf("TEEC_OpenSession failed: 0x%x\n", tee_rv);
-		goto end_2;
-	}
-
-
-	///////////////////
-	///// Hashing /////
-	///////////////////
-	in_mem.buffer = data;
-	in_mem.size = BITCOIN_MESSAGE_LEN;
-	in_mem.flags = TEEC_MEM_INPUT;
-
-	tee_rv = TEEC_RegisterSharedMemory(&context, &in_mem);
-	if (tee_rv != TEE_SUCCESS) {
-		printf("Failed to register IN shared memory\n");
-		goto end_3;
-	}
-
-	out_mem.buffer = sha256;
-	out_mem.size = SHA256_SIZE;
-	out_mem.flags = TEEC_MEM_OUTPUT;
-
-	tee_rv = TEEC_RegisterSharedMemory(&context, &out_mem);
-	if (tee_rv != TEE_SUCCESS) {
-		printf("Failed to allocate OUT shared memory\n");
-		goto end_3;
-	}
-
-	operation.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_MEMREF_WHOLE,
-						TEEC_MEMREF_WHOLE, TEEC_NONE);
-	operation.params[0].value.a = NETWORK_BITCOIN;
-	operation.params[1].memref.parent = &in_mem;
-	operation.params[2].memref.parent = &out_mem;
-
-	printf("Invoking command: Do final Double sha256: ");
-	tee_rv = TEEC_InvokeCommand(&session, HASH_DO_FINAL, &operation, NULL);
-	if (tee_rv != TEEC_SUCCESS) {
-		printf("Hash TEEC_InvokeCommand failed: 0x%x\n", tee_rv);
-		goto end_4;
-	} else {
-		printf("Hash Done\n");
-	}
-
-	/*
-	 * Printf sha256 buf
-	 */
-	printf("Calculated sha256: ");
-	for (i = 0; i < SHA256_SIZE; i++)
-		printf("%02x", sha256[i]);
-	printf("\n");
+   /*
+    * Open session towards Digest TA
+    */
+   tee_rv = TEEC_OpenSession(&context, &session,
+              &uuid, TEEC_LOGIN_PUBLIC,
+              NULL, NULL, NULL);
+   if (tee_rv != TEEC_SUCCESS) {
+      printf("TEEC_OpenSession failed: 0x%x\n", tee_rv);
+      goto end_2;
+   }
 
 
-	///////////////////
-	///// Signing /////
-	///////////////////
-	sign_input_mem.buffer = sha256;
-	sign_input_mem.size = SHA256_SIZE;
-	sign_input_mem.flags = TEEC_MEM_INPUT;
+   ///////////////////
+   ///// Hashing /////
+   ///////////////////
+   in_mem.buffer = data;
+   in_mem.size = BITCOIN_MESSAGE_LEN;
+   in_mem.flags = TEEC_MEM_INPUT;
 
-	tee_rv = TEEC_RegisterSharedMemory(&context, &sign_input_mem);
-	if (tee_rv != TEEC_SUCCESS) {
-		printf("Failed to register IN shared memory for signing\n");
-		goto end_4;
-	}
+   tee_rv = TEEC_RegisterSharedMemory(&context, &in_mem);
+   if (tee_rv != TEE_SUCCESS) {
+      printf("Failed to register IN shared memory\n");
+      goto end_3;
+   }
 
-	sign_output_mem.buffer = sig;
-	sign_output_mem.size = sizeof(sig);
-	sign_output_mem.flags = TEEC_MEM_OUTPUT;
+   out_mem.buffer = sha256;
+   out_mem.size = SHA256_SIZE;
+   out_mem.flags = TEEC_MEM_OUTPUT;
 
-	tee_rv = TEEC_RegisterSharedMemory(&context, &sign_output_mem);
-	if (tee_rv != TEEC_SUCCESS) {
-		printf("Failed to register OUT shared memory for signature\n");
-		goto end_4;
-	}
-	
+   tee_rv = TEEC_RegisterSharedMemory(&context, &out_mem);
+   if (tee_rv != TEE_SUCCESS) {
+      printf("Failed to allocate OUT shared memory\n");
+      goto end_3;
+   }
 
-	operation.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_MEMREF_WHOLE,
-						TEEC_MEMREF_WHOLE, TEEC_NONE);
-	operation.params[0].value.a = NETWORK_BITCOIN;
-	operation.params[1].memref.parent = &sign_input_mem;
-	operation.params[2].memref.parent = &sign_output_mem;
-	tee_rv = TEEC_InvokeCommand(&session, SIGN_DO_FINAL, &operation, NULL);
-	if (tee_rv != TEEC_SUCCESS) {
-		printf("Sign TEEC_InvokeCommand failed: 0x%x\n", tee_rv);
-		goto end_4;
-	}
+   operation.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_MEMREF_WHOLE,
+                  TEEC_MEMREF_WHOLE, TEEC_NONE);
+   operation.params[0].value.a = NETWORK_BITCOIN;
+   operation.params[1].memref.parent = &in_mem;
+   operation.params[2].memref.parent = &out_mem;
 
-	// Signature 출력
-	printf("Signature: ");
-	for (i = 0; i < sign_output_mem.size; i++) {
-		printf("%02x", sig[i]);
-	}
-	printf("\n");
+   printf("Invoking command: Do final Double sha256: ");
+   tee_rv = TEEC_InvokeCommand(&session, HASH_DO_FINAL, &operation, NULL);
+   if (tee_rv != TEEC_SUCCESS) {
+      printf("Hash TEEC_InvokeCommand failed: 0x%x\n", tee_rv);
+      goto end_4;
+   } else {
+      printf("Hash Done\n");
+   }
 
-	// 개인키, 공개키 출력
+   /*
+    * Printf sha256 buf
+    */
+   printf("Calculated sha256: ");
+   for (i = 0; i < SHA256_SIZE; i++)
+      printf("%02x", sha256[i]);
+   printf("\n");
 
-	// key.buffer = fuck;
-	// key.size = sizeof(fuck);
-	// key.flags = TEEC_MEM_OUTPUT;
+
+   ///////////////////
+   ///// Signing /////
+   ///////////////////
+   sign_input_mem.buffer = sha256;
+   sign_input_mem.size = SHA256_SIZE;
+   sign_input_mem.flags = TEEC_MEM_INPUT;
+
+   tee_rv = TEEC_RegisterSharedMemory(&context, &sign_input_mem);
+   if (tee_rv != TEEC_SUCCESS) {
+      printf("Failed to register IN shared memory for signing\n");
+      goto end_4;
+   }
+
+   sign_output_mem.buffer = sig;
+   sign_output_mem.size = sizeof(sig);
+   sign_output_mem.flags = TEEC_MEM_OUTPUT;
+
+   tee_rv = TEEC_RegisterSharedMemory(&context, &sign_output_mem);
+   if (tee_rv != TEEC_SUCCESS) {
+      printf("Failed to register OUT shared memory for signature\n");
+      goto end_4;
+   }
+   
+
+   operation.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_MEMREF_WHOLE,
+                  TEEC_MEMREF_WHOLE, TEEC_NONE);
+   operation.params[0].value.a = NETWORK_BITCOIN;
+   operation.params[1].memref.parent = &sign_input_mem;
+   operation.params[2].memref.parent = &sign_output_mem;
+   tee_rv = TEEC_InvokeCommand(&session, SIGN_DO_FINAL, &operation, NULL);
+   if (tee_rv != TEEC_SUCCESS) {
+      printf("Sign TEEC_InvokeCommand failed: 0x%x\n", tee_rv);
+      goto end_4;
+   }
+
+   // Signature 출력
+   printf("Signature: ");
+   for (i = 0; i < sign_output_mem.size; i++) {
+      printf("%02x", sig[i]);
+   }
+   printf("\n");
+
+   // 개인키, 공개키 출력
+
+   // key.buffer = fuck;
+   // key.size = sizeof(fuck);
+   // key.flags = TEEC_MEM_OUTPUT;
 
     // tee_rv = TEEC_RegisterSharedMemory(&context, &key);
     // if (tee_rv != TEEC_SUCCESS) {
@@ -210,33 +210,33 @@ int main()
     //     goto end_1;
     // }
 
-	// memset((void *)&operation, 0, sizeof(operation));
+   // memset((void *)&operation, 0, sizeof(operation));
 
-	// operation.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_WHOLE, TEEC_NONE, TEEC_NONE, TEEC_NONE);
+   // operation.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_WHOLE, TEEC_NONE, TEEC_NONE, TEEC_NONE);
     // operation.params[0].memref.parent = &key;
     // operation.params[0].memref.size = key.size;
 
-	// tee_rv = TEEC_InvokeCommand(&session, DUMP_KEY, &operation, NULL);
+   // tee_rv = TEEC_InvokeCommand(&session, DUMP_KEY, &operation, NULL);
     // if (tee_rv != TEEC_SUCCESS) {
     //     printf("Invoke failed: 0x%x\n", tee_rv);
     //     goto end_1;
     // }
 
-	// print_hex("Private d", key.buffer, 32);
+   // print_hex("Private d", key.buffer, 32);
     // print_hex("Public X", (uint8_t *)key.buffer + 32, 32);
     // print_hex("Public Y", (uint8_t *)key.buffer + 64, 32);
 
 
-	
+   
 end_4:
-	TEEC_ReleaseSharedMemory(&out_mem);
+   TEEC_ReleaseSharedMemory(&out_mem);
 
 end_3:
-	TEEC_ReleaseSharedMemory(&in_mem);
-	TEEC_CloseSession(&session);
+   TEEC_ReleaseSharedMemory(&in_mem);
+   TEEC_CloseSession(&session);
 end_2:
-	TEEC_FinalizeContext(&context);
+   TEEC_FinalizeContext(&context);
 end_1:
-	exit(tee_rv);
+   exit(tee_rv);
 }
 
--- TAs/example_conversion/sign_ecdsa_256.c
+++ TAs/example_conversion/sign_ecdsa_256.c
@@ -32,19 +32,19 @@
 
 /* Hash TA command IDs for this applet */
 #define HASH_DO_FINAL 0x00000001
-#define SIGN_DO_FINAL	0x00000002
-#define DUMP_KEY 0XDEAD
+#define SIGN_DO_FINAL   0x00000002
+#define VERIFY_SIGNATURE 0XDEAD
 /* Blockchain Network */
 #define NETWORK_BITCOIN 0x0
 #define NETWORK_ETHEREUM 0x1
 
 SET_TA_PROPERTIES(
-	{ 0x12345678, 0x8765, 0x4321, { 'S', 'I', 'G', 'N', 'S', 'I', 'G', 'N'} }, /* UUID */
-		512, /* dataSize */
-		255, /* stackSize */
-		1, /* singletonInstance */
-		1, /* multiSession */
-		1) /* instanceKeepAlive */
+   { 0x12345678, 0x8765, 0x4321, { 'S', 'I', 'G', 'N', 'S', 'I', 'G', 'N'} }, /* UUID */
+      512, /* dataSize */
+      255, /* stackSize */
+      1, /* singletonInstance */
+      1, /* multiSession */
+      1) /* instanceKeepAlive */
 #endif
 
 #define DUMP_KEY 0XDEAD
@@ -59,238 +59,263 @@ void print_hex(const char *label, const uint8_t *data, size_t len) {
 
 TEE_Result TA_EXPORT TA_CreateEntryPoint(void)
 {
-	char objID[] = "signkey";
-	uint32_t objID_len = 7;
-	TEE_ObjectHandle signkey = NULL;
-	TEE_Result rv = TEE_ERROR_GENERIC;
-	TEE_Attribute params = {0};
-	TEE_OperationHandle sign_operation = NULL;
+   char objID[] = "signkey";
+   uint32_t objID_len = 7;
+   TEE_ObjectHandle signkey = NULL;
+   TEE_Result rv = TEE_ERROR_GENERIC;
+   TEE_Attribute params = {0};
+   TEE_OperationHandle sign_operation = NULL;
 
-	OT_LOG(LOG_ERR, "Calling the create entry point");
-	
-	rv = TEE_AllocateTransientObject(TEE_TYPE_ECDSA_KEYPAIR, 256, &signkey);
-	if (rv != TEE_SUCCESS) {
-		OT_LOG(LOG_ERR, "Transient object alloc failed [0x%x]", rv);
-		goto out;
-	}
-	
-	params.attributeID = TEE_ATTR_ECC_CURVE;
-	params.content.value.a = TEE_ECC_CURVE_NIST_P256;
-	
-	rv = TEE_GenerateKey(signkey, 256, &params, 1);
-	if (rv != TEE_SUCCESS) {
-		OT_LOG(LOG_ERR, "Key generation failed [0x%x]", rv);
-		goto out;
-	}
+   OT_LOG(LOG_ERR, "Calling the create entry point");
+   
+   rv = TEE_AllocateTransientObject(TEE_TYPE_ECDSA_KEYPAIR, 256, &signkey);
+   if (rv != TEE_SUCCESS) {
+      OT_LOG(LOG_ERR, "Transient object alloc failed [0x%x]", rv);
+      goto out;
+   }
+   
+   params.attributeID = TEE_ATTR_ECC_CURVE;
+   params.content.value.a = TEE_ECC_CURVE_NIST_P256;
+   
+   rv = TEE_GenerateKey(signkey, 256, &params, 1);
+   if (rv != TEE_SUCCESS) {
+      OT_LOG(LOG_ERR, "Key generation failed [0x%x]", rv);
+      goto out;
+   }
 
-	// PrintOut private key
-	uint8_t privkey[32];
+   // PrintOut private key
+   uint8_t privkey[32];
     size_t len = sizeof(privkey);
-	rv = TEE_GetObjectBufferAttribute(signkey, TEE_ATTR_ECC_PRIVATE_VALUE, privkey, &len);
+   rv = TEE_GetObjectBufferAttribute(signkey, TEE_ATTR_ECC_PRIVATE_VALUE, privkey, &len);
     if (rv == TEE_SUCCESS) {
         OT_LOG(LOG_ERR, "Private key: ");
         for (int i = 0; i < len; i++)
             OT_LOG(LOG_ERR, "%02x", privkey[i]);
-		OT_LOG(LOG_ERR, "\n");
+      OT_LOG(LOG_ERR, "\n");
     } else {
         OT_LOG(LOG_ERR, "Failed to get private key: 0x%x", rv);
-		goto out;
+      goto out;
     }
 
-	print_hex("Private Key: ", privkey, 32);
+   print_hex("Private Key: ", privkey, 32);
 
-	// 공개키 X, Y 추출
-	uint8_t pubkey_x[32], pubkey_y[32];
-	size_t x_len = sizeof(pubkey_x), y_len = sizeof(pubkey_y);
+   // 공개키 X, Y 추출
+   uint8_t pubkey_x[32], pubkey_y[32];
+   size_t x_len = sizeof(pubkey_x), y_len = sizeof(pubkey_y);
 
-	rv = TEE_GetObjectBufferAttribute(signkey, TEE_ATTR_ECC_PUBLIC_VALUE_X, pubkey_x, &x_len);
-	if (rv != TEE_SUCCESS) {
-		OT_LOG(LOG_ERR, "Failed to get public key X: 0x%x", rv);
-		goto out;
-	}
+   rv = TEE_GetObjectBufferAttribute(signkey, TEE_ATTR_ECC_PUBLIC_VALUE_X, pubkey_x, &x_len);
+   if (rv != TEE_SUCCESS) {
+      OT_LOG(LOG_ERR, "Failed to get public key X: 0x%x", rv);
+      goto out;
+   }
 
-	rv = TEE_GetObjectBufferAttribute(signkey, TEE_ATTR_ECC_PUBLIC_VALUE_Y, pubkey_y, &y_len);
-	if (rv != TEE_SUCCESS) {
-		OT_LOG(LOG_ERR, "Failed to get public key Y: 0x%x", rv);
-		goto out;
-	}
+   rv = TEE_GetObjectBufferAttribute(signkey, TEE_ATTR_ECC_PUBLIC_VALUE_Y, pubkey_y, &y_len);
+   if (rv != TEE_SUCCESS) {
+      OT_LOG(LOG_ERR, "Failed to get public key Y: 0x%x", rv);
+      goto out;
+   }
 
-	printf("Public key X: ");
-	for (int i = 0; i < x_len; i++)
-		printf("%02x", pubkey_x[i]);
-	printf("\n");
+   printf("Public key X: ");
+   for (int i = 0; i < x_len; i++)
+      printf("%02x", pubkey_x[i]);
+   printf("\n");
 
-	printf("Public key Y: ");
-	for (int i = 0; i < y_len; i++)
-		printf("%02x", pubkey_y[i]);
-	printf("\n");
-	
-	rv = TEE_CreatePersistentObject(TEE_STORAGE_PRIVATE,
-					objID, objID_len,
-					TEE_DATA_FLAG_ACCESS_WRITE | TEE_DATA_FLAG_EXCLUSIVE,
-					signkey,
-					NULL, 0, NULL);
-	if (rv != TEE_SUCCESS) {
-		OT_LOG(LOG_ERR, "Persistent object create failed [0x%x]", rv);
-		goto out;
-	}
-			
-	rv = TEE_AllocateOperation(&sign_operation,
-				   TEE_ALG_ECDSA_SHA256, TEE_MODE_SIGN, 256);
-	if (rv != TEE_SUCCESS) {
-		OT_LOG(LOG_ERR, "Operation allocation failed [0x%x]", rv);
-		goto out;
-	}
+   printf("Public key Y: ");
+   for (int i = 0; i < y_len; i++)
+      printf("%02x", pubkey_y[i]);
+   printf("\n");
+   
+   rv = TEE_CreatePersistentObject(TEE_STORAGE_PRIVATE,
+               objID, objID_len,
+               TEE_DATA_FLAG_ACCESS_WRITE | TEE_DATA_FLAG_EXCLUSIVE,
+               signkey,
+               NULL, 0, NULL);
+   if (rv != TEE_SUCCESS) {
+      OT_LOG(LOG_ERR, "Persistent object create failed [0x%x]", rv);
+      goto out;
+   }
+         
+   rv = TEE_AllocateOperation(&sign_operation,
+               TEE_ALG_ECDSA_SHA256, TEE_MODE_SIGN, 256);
+   if (rv != TEE_SUCCESS) {
+      OT_LOG(LOG_ERR, "Operation allocation failed [0x%x]", rv);
+      goto out;
+   }
 
-	rv = TEE_SetOperationKey(sign_operation, signkey);
-	if (rv != TEE_SUCCESS) {
-		OT_LOG(LOG_ERR, "Operation key set failed [0x%x]", rv);
-		goto out;
-	}
+   rv = TEE_SetOperationKey(sign_operation, signkey);
+   if (rv != TEE_SUCCESS) {
+      OT_LOG(LOG_ERR, "Operation key set failed [0x%x]", rv);
+      goto out;
+   }
 
-	TEE_SetInstanceData(sign_operation);
-	
+   TEE_SetInstanceData(sign_operation);
+   
  out:
-	if (rv != TEE_SUCCESS)
-		TEE_FreeOperation(sign_operation);
-	TEE_FreeTransientObject(signkey);
-	return rv;
+   if (rv != TEE_SUCCESS)
+      TEE_FreeOperation(sign_operation);
+   TEE_FreeTransientObject(signkey);
+   return rv;
 }
 
 void TA_EXPORT TA_DestroyEntryPoint(void)
 {
-	TEE_FreeOperation(TEE_GetInstanceData());
+   TEE_FreeOperation(TEE_GetInstanceData());
 }
 
 TEE_Result TA_EXPORT TA_OpenSessionEntryPoint(uint32_t paramTypes,
-					      TEE_Param params[4],
-					      void **sessionContext)
+                     TEE_Param params[4],
+                     void **sessionContext)
 {
-	paramTypes = paramTypes;
-	params = params;
-	sessionContext = sessionContext;
-	
-	return TEE_SUCCESS;
+   paramTypes = paramTypes;
+   params = params;
+   sessionContext = sessionContext;
+   
+   return TEE_SUCCESS;
 }
 
 void TA_EXPORT TA_CloseSessionEntryPoint(void *sessionContext)
 {
-	sessionContext = sessionContext;
+   sessionContext = sessionContext;
 }
 
 TEE_Result TA_EXPORT TA_InvokeCommandEntryPoint(void *sessionContext,
-						uint32_t commandID,
-						uint32_t paramTypes,
-						TEE_Param params[4])
+                  uint32_t commandID,
+                  uint32_t paramTypes,
+                  TEE_Param params[4])
 {
-	TEE_Result tee_rv = TEEC_SUCCESS;
+   TEE_Result tee_rv = TEEC_SUCCESS;
 
-	sessionContext = sessionContext;
+   sessionContext = sessionContext;
 
-	TEE_OperationHandle op;
-	uint8_t temp_hash[32];
-	size_t temp_len = sizeof(temp_hash);
-	uint8_t d[32], x[32], y[32];
-	uint32_t d_len = 32;
-	uint32_t x_len = 32;
-	uint32_t y_len = 32;
+   TEE_OperationHandle op;
+   uint8_t temp_hash[32];
+   size_t temp_len = sizeof(temp_hash);
+   uint8_t d[32], x[32], y[32];
+   uint32_t d_len = 32;
+   uint32_t x_len = 32;
+   uint32_t y_len = 32;
 
-	TEE_ObjectHandle signkey = NULL;
+   TEE_ObjectHandle signkey = NULL;
 
-	// if (TEE_PARAM_TYPE_GET(paramTypes, 0) != TEE_PARAM_TYPE_VALUE_INPUT ||
-	// 	TEE_PARAM_TYPE_GET(paramTypes, 1) != TEE_PARAM_TYPE_MEMREF_INPUT ||
-	//     TEE_PARAM_TYPE_GET(paramTypes, 2) != TEE_PARAM_TYPE_MEMREF_OUTPUT) {
-	// 	OT_LOG(LOG_ERR, "Bad parameter at index 1 OR 2");
-	// 	return TEE_ERROR_BAD_PARAMETERS;
-	// }
+   // if (TEE_PARAM_TYPE_GET(paramTypes, 0) != TEE_PARAM_TYPE_VALUE_INPUT ||
+   //    TEE_PARAM_TYPE_GET(paramTypes, 1) != TEE_PARAM_TYPE_MEMREF_INPUT ||
+   //     TEE_PARAM_TYPE_GET(paramTypes, 2) != TEE_PARAM_TYPE_MEMREF_OUTPUT) {
+   //    OT_LOG(LOG_ERR, "Bad parameter at index 1 OR 2");
+   //    return TEE_ERROR_BAD_PARAMETERS;
+   // }
 
-	switch (commandID) {
-		case HASH_DO_FINAL:
-			switch (params[0].value.a) {
-				case NETWORK_BITCOIN:
+   switch (commandID) {
+      case HASH_DO_FINAL:
+         switch (params[0].value.a) {
+            case NETWORK_BITCOIN:{
 
-					tee_rv = TEE_AllocateOperation(&op, TEE_ALG_SHA256, TEE_MODE_DIGEST, 0);
-					if (tee_rv != TEE_SUCCESS) {
-						OT_LOG(LOG_ERR, "[NETWORK_BITCOIN] TEE_AllocateOperation failed [0x%x]", tee_rv);
-						goto out;
-					}
+               TEE_OperationHandle op = NULL;
+               uint8_t first_hash[32];
+               size_t first_hash_len = sizeof(first_hash);
 
-					tee_rv = TEE_DigestDoFinal(op, params[1].memref.buffer, params[1].memref.size, params[2].memref.buffer, &params[2].memref.size);
-					if (tee_rv != TEE_SUCCESS) {
-						OT_LOG(LOG_ERR, "[NETWORK_BITCOIN] First TEE_DigestDoFinal failed [0x%x]", tee_rv);
-						goto out;
-					}
+               // 1차 해시
+               tee_rv = TEE_AllocateOperation(&op, TEE_ALG_SHA256, TEE_MODE_DIGEST, 0);
+               if (tee_rv != TEE_SUCCESS) {
+                  OT_LOG(LOG_ERR, "[NETWORK_BITCOIN] TEE_AllocateOperation failed [0x%x]", tee_rv);
+                  goto out;
+               }
 
-					TEE_FreeOperation(op);
-					break;
-				case NETWORK_ETHEREUM:
-					break;
-			default:
-				break;
-		}
-		break;
-	case SIGN_DO_FINAL:
-		switch (params[0].value.a) {
-			case NETWORK_BITCOIN:
-				tee_rv = TEE_AsymmetricSignDigest(TEE_GetInstanceData(), NULL, 0,
-							params[1].memref.buffer, params[1].memref.size,
-							params[2].memref.buffer, &params[2].memref.size);
-				if (tee_rv != TEE_SUCCESS) {
-					OT_LOG(LOG_ERR, "Sign failed");
-					goto out;
-				}
-				break;
-			case NETWORK_ETHEREUM:
-				break;
-			default:
-				break;
-		}
-		break;
-	// case DUMP_KEY:
-	// 		printf("FUck~!!!!\n");
-	// 		tee_rv = TEE_OpenPersistentObject(TEE_STORAGE_PRIVATE,
-	// 									"signkey", 7,
-	// 									TEE_DATA_FLAG_ACCESS_READ,
-	// 									&signkey);
-	// 		if (tee_rv != TEE_SUCCESS){
-	// 			OT_LOG(LOG_ERR, "Failed on PRIVATE_VALUE: 0x%x", tee_rv);
-	// 			return 1;
-	// 		}
+               tee_rv = TEE_DigestDoFinal(op,
+                                    params[1].memref.buffer, params[1].memref.size,
+                                    first_hash, &first_hash_len);
+               if (tee_rv != TEE_SUCCESS) {
+                  OT_LOG(LOG_ERR, "[NETWORK_BITCOIN] First TEE_DigestDoFinal failed [0x%x]", tee_rv);
+                  goto out;
+               }
 
-	// 		tee_rv = TEE_GetObjectValueAttribute(signkey, TEE_ATTR_ECC_PRIVATE_VALUE, d, &d_len);
-	// 		if (tee_rv != TEE_SUCCESS){
-	// 			return 2;
-	// 			OT_LOG(LOG_ERR, "Failed on PRIVATE_VALUE: 0x%x", tee_rv);
-	// 		}
+               TEE_FreeOperation(op);
+               // 2차 해시
+               tee_rv = TEE_AllocateOperation(&op, TEE_ALG_SHA256, TEE_MODE_DIGEST, 0);
+               if (tee_rv != TEE_SUCCESS) {
+                  OT_LOG(LOG_ERR, "[NETWORK_BITCOIN] Second TEE_AllocateOperation failed [0x%x]", tee_rv);
+                  goto out;
+               }
 
-	// 		tee_rv = TEE_GetObjectValueAttribute(signkey, TEE_ATTR_ECC_PUBLIC_VALUE_X, x, &x_len);
-	// 		if (tee_rv != TEE_SUCCESS){ 
-	// 			OT_LOG(LOG_ERR, "Failed on PRIVATE_VALUE: 0x%x", tee_rv);
-	// 			return 3;
-	// 		}
+               tee_rv = TEE_DigestDoFinal(op,
+                                    first_hash, first_hash_len,
+                                    params[2].memref.buffer, &params[2].memref.size);
+               if (tee_rv != TEE_SUCCESS) {
+                  OT_LOG(LOG_ERR, "[NETWORK_BITCOIN] Second TEE_DigestDoFinal failed [0x%x]", tee_rv);
+                  goto out;
+               }
 
-	// 		tee_rv = TEE_GetObjectValueAttribute(signkey, TEE_ATTR_ECC_PUBLIC_VALUE_Y, y, &y_len);
-	// 		if (tee_rv != TEE_SUCCESS){ 
-	// 			OT_LOG(LOG_ERR, "Failed on PRIVATE_VALUE: 0x%x", tee_rv);
-	// 			return 4;
-	// 		}
+               TEE_FreeOperation(op);
+               
+               break;
+            }
+            case NETWORK_ETHEREUM:
+               break;
+         default:
+            break;
+      }
+      break;
+   case SIGN_DO_FINAL:
+      switch (params[0].value.a) {
+         case NETWORK_BITCOIN:
+            tee_rv = TEE_AsymmetricSignDigest(TEE_GetInstanceData(), NULL, 0,
+                     params[1].memref.buffer, params[1].memref.size,
+                     params[2].memref.buffer, &params[2].memref.size);
+            if (tee_rv != TEE_SUCCESS) {
+               OT_LOG(LOG_ERR, "Sign failed");
+               goto out;
+            }
+            break;
+         case NETWORK_ETHEREUM:
+            break;
+         default:
+            break;
+      }
+      break;
+   // case DUMP_KEY:
+   //       printf("FUck~!!!!\n");
+   //       tee_rv = TEE_OpenPersistentObject(TEE_STORAGE_PRIVATE,
+   //                            "signkey", 7,
+   //                            TEE_DATA_FLAG_ACCESS_READ,
+   //                            &signkey);
+   //       if (tee_rv != TEE_SUCCESS){
+   //          OT_LOG(LOG_ERR, "Failed on PRIVATE_VALUE: 0x%x", tee_rv);
+   //          return 1;
+   //       }
 
-	// 		if (params[0].memref.size < 96){
-	// 			OT_LOG(LOG_ERR, "Failed on PRIVATE_VALUE: 0x%x", tee_rv);
-	// 			return 5;
-	// 		}
+   //       tee_rv = TEE_GetObjectValueAttribute(signkey, TEE_ATTR_ECC_PRIVATE_VALUE, d, &d_len);
+   //       if (tee_rv != TEE_SUCCESS){
+   //          return 2;
+   //          OT_LOG(LOG_ERR, "Failed on PRIVATE_VALUE: 0x%x", tee_rv);
+   //       }
 
-	// 		TEE_MemMove(params[0].memref.buffer, d, 32);
-	// 		TEE_MemMove((uint8_t *)params[0].memref.buffer + 32, x, 32);
-	// 		TEE_MemMove((uint8_t *)params[0].memref.buffer + 64, y, 32);
+   //       tee_rv = TEE_GetObjectValueAttribute(signkey, TEE_ATTR_ECC_PUBLIC_VALUE_X, x, &x_len);
+   //       if (tee_rv != TEE_SUCCESS){ 
+   //          OT_LOG(LOG_ERR, "Failed on PRIVATE_VALUE: 0x%x", tee_rv);
+   //          return 3;
+   //       }
 
-	// 		params[0].memref.size = 96;
-	// 		return tee_rv;
-	// 		break;
-	default:
-		tee_rv = TEE_ERROR_BAD_PARAMETERS;
-	}
+   //       tee_rv = TEE_GetObjectValueAttribute(signkey, TEE_ATTR_ECC_PUBLIC_VALUE_Y, y, &y_len);
+   //       if (tee_rv != TEE_SUCCESS){ 
+   //          OT_LOG(LOG_ERR, "Failed on PRIVATE_VALUE: 0x%x", tee_rv);
+   //          return 4;
+   //       }
+
+   //       if (params[0].memref.size < 96){
+   //          OT_LOG(LOG_ERR, "Failed on PRIVATE_VALUE: 0x%x", tee_rv);
+   //          return 5;
+   //       }
+
+   //       TEE_MemMove(params[0].memref.buffer, d, 32);
+   //       TEE_MemMove((uint8_t *)params[0].memref.buffer + 32, x, 32);
+   //       TEE_MemMove((uint8_t *)params[0].memref.buffer + 64, y, 32);
+
+   //       params[0].memref.size = 96;
+   //       return tee_rv;
+   //       break;
+   default:
+      tee_rv = TEE_ERROR_BAD_PARAMETERS;
+   }
 
  out:
-	return tee_rv;
+   return tee_rv;
 }
